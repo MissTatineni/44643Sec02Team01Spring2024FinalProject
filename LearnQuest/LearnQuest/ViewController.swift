0e0b1ad9399d69ae1ff8aa3ebbb752934729e2fe
eluvakathirupathi <eluvakathirupathi@gmail.com>
2024-02-22 14:05:43 -0600

    Thirupathi Eluvaka: Added elements in home page and assets

--- LearnQuest/LearnQuest/ViewController.swift
+++ LearnQuest/LearnQuest/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Lottie
+import AnimatedGradientView
 
 class ViewController: UIViewController {
 
@@ -14,18 +15,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    override func viewIsAppearing(_ animated: Bool) {
+        self.applyGradientBackground()
+    }
 
-    @IBOutlet weak var LaunchLAV: LottieAnimationView!{
-        didSet{
-            LaunchLAV.animation = LottieAnimation.named("launch")
-            LaunchLAV.animationSpeed = 1.0
-            LaunchLAV.loopMode = .playOnce
-            LaunchLAV.play{ [weak self] _ in
-                UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.0, delay: 0.0,options: [.curveEaseInOut]){
-                    self?.LaunchLAV.alpha = 0.0
-                }
-            }
-        }
+//    @IBOutlet weak var LaunchLAV: LottieAnimationView!{
+//        didSet{
+//            LaunchLAV.animation = LottieAnimation.named("launch")
+//            LaunchLAV.animationSpeed = 1.0
+//            LaunchLAV.loopMode = .playOnce
+//            LaunchLAV.play{ [weak self] _ in
+//                UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.0, delay: 0.0,options: [.curveEaseInOut]){
+//                    self?.LaunchLAV.alpha = 0.0
+//                }
+//            }
+//        }
+//    }
+    
+    private func applyGradientBackground(){
+        let gradientType: CAGradientLayerType = .axial
+        let direction: AnimatedGradientViewDirection = .up
+        let gradient = AnimatedGradientView(frame: self.view.bounds)
+        gradient.animationValues = [
+            (colors: ["#90EE90", "#DCD3F3"], direction, gradientType),
+            (colors: ["#ADD8E6", "#E9F3F2"], direction, gradientType),
+            (colors: ["#DCD3F3", "#90EE90"], direction, gradientType),
+            (colors: ["#E9F3F2", "#ADD8E6"], direction,gradientType)
+        ]
+        self.view.insertSubview(gradient, at: 0)
+                                 
     }
     
 }
