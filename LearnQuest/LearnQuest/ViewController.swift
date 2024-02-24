51b2972c6e488b6359fcdcf0eb24ae4753f663c6
eluvakathirupathi <eluvakathirupathi@gmail.com>
2024-02-23 23:05:35 -0600

    Eluvaka Thirupathi: UI for user login page is done

--- LearnQuest/LearnQuest/ViewController.swift
+++ LearnQuest/LearnQuest/ViewController.swift
@@ -19,18 +19,18 @@ class ViewController: UIViewController {
         self.applyGradientBackground()
     }
 
-//    @IBOutlet weak var LaunchLAV: LottieAnimationView!{
-//        didSet{
-//            LaunchLAV.animation = LottieAnimation.named("launch")
-//            LaunchLAV.animationSpeed = 1.0
-//            LaunchLAV.loopMode = .playOnce
-//            LaunchLAV.play{ [weak self] _ in
-//                UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.0, delay: 0.0,options: [.curveEaseInOut]){
-//                    self?.LaunchLAV.alpha = 0.0
-//                }
-//            }
-//        }
-//    }
+    @IBOutlet weak var LaunchLAV: LottieAnimationView!{
+        didSet{
+            LaunchLAV.animation = LottieAnimation.named("launch")
+            LaunchLAV.animationSpeed = 1.0
+            LaunchLAV.loopMode = .playOnce
+            LaunchLAV.play{ [weak self] _ in
+                UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1.0, delay: 0.0,options: [.curveEaseInOut]){
+                    self?.LaunchLAV.alpha = 0.0
+                }
+            }
+        }
+    }
     
     private func applyGradientBackground(){
         let gradientType: CAGradientLayerType = .axial
