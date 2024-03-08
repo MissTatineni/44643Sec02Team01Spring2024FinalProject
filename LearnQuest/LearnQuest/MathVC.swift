6066479d8a2c5cba64eca0b00c1c293fc2add419
Prameela2511 <S566983@nwmissouri.edu>
2024-03-08 15:44:58 -0800

    Prameela : added outlets and created cocoa touch class(started working on it)

---
+++ LearnQuest/LearnQuest/MathVC.swift
@@ -0,0 +1,35 @@
+//
+//  MathVC.swift
+//  LearnQuest
+//
+//  Created by Prameela on 3/8/24.
+//
+
+import UIKit
+
+class MathVC: UIViewController {
+
+    @IBAction func Level02(_ sender: UIButton) {
+    }
+    @IBAction func Level01(_ sender: UIButton) {
+    }
+    @IBAction func Level03(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
