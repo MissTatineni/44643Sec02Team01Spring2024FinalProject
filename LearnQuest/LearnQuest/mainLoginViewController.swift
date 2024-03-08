ef606e10df968a60b26d68597731269c1ef9fe02
Prameela2511 <S566983@nwmissouri.edu>
2024-03-08 13:57:48 -0800

    Pathuri Prameela added outlets and actions with cocoa touch classes

---
+++ LearnQuest/LearnQuest/mainLoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  mainLoginViewController.swift
+//  LearnQuest
+//
+//  Created by Prameela on 3/8/24.
+//
+
+import UIKit
+
+class mainLoginViewController: UIViewController {
+    
+    @IBAction func login(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
