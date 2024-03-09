6825ea24dc1d475e8a5c917e564d4a3c0d70530d
Bhargavi Tatineni <s566608@nwmissouri.edu>
2024-03-08 17:33:44 -0800

    Bhargavi Tatineni: Notifications functionality added

---
+++ LearnQuest/LearnQuest/UserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UserViewController.swift
+//  LearnQuest
+//
+//  Created by Bhargavi Tatineni on 3/8/24.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
