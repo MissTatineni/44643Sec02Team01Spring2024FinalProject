a302e88d39600327f3d01af7b3a20b5a57dbba21
Prameela2511 <S566983@nwmissouri.edu>
2024-03-29 20:10:36 -0500

    Prameela Pathuri : AnimatedGradient view added in all screens.

--- LearnQuest/LearnQuest/DisplayScore.swift
+++ LearnQuest/LearnQuest/DisplayScore.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AnimatedGradientView
 
 class DisplayScore: UIViewController {
 
@@ -23,6 +24,20 @@ class DisplayScore: UIViewController {
     @IBAction func ChangeAvator(_ sender: UIButton) {
     }
     
+    
+    private func applyGradientBackground(){
+        let gradientType: CAGradientLayerType = .axial
+        let direction: AnimatedGradientViewDirection = .up
+        let gradient = AnimatedGradientView(frame: self.view.bounds)
+        gradient.animationValues = [
+            (colors: ["#90EE90", "#DCD3F3"], direction, gradientType),
+            (colors: ["#ADD8E6", "#E9F3F2"], direction, gradientType),
+            (colors: ["#DCD3F3", "#90EE90"], direction, gradientType),
+            (colors: ["#E9F3F2", "#ADD8E6"], direction,gradientType)
+        ]
+        self.view.insertSubview(gradient, at: 0)
+        
+    }
     /*
     // MARK: - Navigation
 
