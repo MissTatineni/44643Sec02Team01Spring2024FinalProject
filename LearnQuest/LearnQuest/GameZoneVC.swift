ae0cc45398a09de8b6c66e7631b689de382fb450
BhargaviTatineni <s566608@nwmissouri.edu>
2024-04-12 00:47:36 -0500

    GameZoneVC

---
+++ LearnQuest/LearnQuest/GameZoneVC.swift
@@ -0,0 +1,29 @@
+//
+//  GameZoneVC.swift
+//  LearnQuest
+//
+//  Created by Bhargavi Tatineni on 4/12/24.
+//
+
+import UIKit
+
+class GameZoneVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
