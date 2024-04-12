946480a1ef4be7892190eedfeae22277815d50f1
BhargaviTatineni <s566608@nwmissouri.edu>
2024-04-12 10:18:45 -0500

    functionality

--- LearnQuest/LearnQuest/GameZoneVC.swift
+++ LearnQuest/LearnQuest/GameZoneVC.swift
@@ -9,12 +9,46 @@ import UIKit
 
 class GameZoneVC: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerTextField: UITextField!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        displayQuestion()
+        
+        
     }
     
+    func displayQuestion() {
+            questionLabel.text = quizQuestions[currentQuestionIndex]
+            feedbackLabel.text = ""
+            answerTextField.text = ""
+        }
+    
+    
+    @IBAction func checkAnswer(_ sender: UIButton) {
+            let userAnswer = answerTextField.text ?? ""
+            let correctAnswer = quizAnswers[currentQuestionIndex]
+            
+            if userAnswer.lowercased() == correctAnswer.lowercased() {
+                feedbackLabel.text = "Correct!"
+            } else {
+                feedbackLabel.text = "Incorrect. The correct answer is \(correctAnswer)."
+            }
+        currentQuestionIndex += 1
+                if currentQuestionIndex < quizQuestions.count {
+                    displayQuestion()
+                } else {
+                    // End of the quiz
+                    currentQuestionIndex = 0 // Reset the question index for next time
+                    let alert = UIAlertController(title: "Congratulations!", message: "You have completed the quiz.", preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                    alert.addAction(okAction)
+                    present(alert, animated: true, completion: nil)
+                }
 
     /*
     // MARK: - Navigation
