aa1e14657799b5a27a3287f6e5fbc106b0b97ca8
Prameela2511 <S566983@nwmissouri.edu>
2024-04-12 19:18:15 -0500

    Prameela : RegisterVC

--- LearnQuest/LearnQuest/RegisterViewController.swift
+++ LearnQuest/LearnQuest/RegisterViewController.swift
@@ -26,15 +26,6 @@ class RegisterViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     @IBAction func register(_ sender: Any) {
         
@@ -90,3 +81,14 @@ extension UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
 }
+
+
+/*
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destination.
+    // Pass the selected object to the new view controller.
+}
+*/
