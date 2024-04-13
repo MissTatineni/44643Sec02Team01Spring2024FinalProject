5065735f14147d1f98d4ebf578b1fdcc61062bc0
BhargaviTatineni <s566608@nwmissouri.edu>
2024-04-12 23:27:50 -0500

    game zone functionality

--- LearnQuest/LearnQuest/GameZoneVC.swift
+++ LearnQuest/LearnQuest/GameZoneVC.swift
@@ -8,47 +8,98 @@
 import UIKit
 
 class GameZoneVC: UIViewController {
-
+    
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerTextField: UITextField!
     @IBOutlet weak var feedbackLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    let quizQuestions = [
+        "What color is the sky?",
+        "How many legs does a cat have?",
+        "What is 2 + 2?"
+    ]
+    
+    let quizAnswers = [
+        "Blue",
+        "Four",
+        "4"
+    ]
+    
+    var currentQuestionIndex = 0
+    var score = 0
+    var timer: Timer?
+    var timeRemaining = 10 // Time in seconds for each question
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         displayQuestion()
-        
-        
+        updateScoreLabel()
+        startTimer()
     }
     
     func displayQuestion() {
-            questionLabel.text = quizQuestions[currentQuestionIndex]
-            feedbackLabel.text = ""
-            answerTextField.text = ""
+        questionLabel.text = quizQuestions[currentQuestionIndex]
+        feedbackLabel.text = ""
+        answerTextField.text = ""
+    }
+    
+    func checkAnswer() {
+        let userAnswer = answerTextField.text ?? ""
+        let correctAnswer = quizAnswers[currentQuestionIndex]
+        
+        if userAnswer.lowercased() == correctAnswer.lowercased() {
+            feedbackLabel.text = "Correct!"
+            score += 1
+        } else {
+            feedbackLabel.text = "Incorrect. The correct answer is \(correctAnswer)."
         }
-    
-    
-    @IBAction func checkAnswer(_ sender: UIButton) {
-            let userAnswer = answerTextField.text ?? ""
-            let correctAnswer = quizAnswers[currentQuestionIndex]
-            
-            if userAnswer.lowercased() == correctAnswer.lowercased() {
-                feedbackLabel.text = "Correct!"
-            } else {
-                feedbackLabel.text = "Incorrect. The correct answer is \(correctAnswer)."
-            }
         currentQuestionIndex += 1
-                if currentQuestionIndex < quizQuestions.count {
-                    displayQuestion()
-                } else {
-                    // End of the quiz
-                    currentQuestionIndex = 0 // Reset the question index for next time
-                    let alert = UIAlertController(title: "Congratulations!", message: "You have completed the quiz.", preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                    alert.addAction(okAction)
-                    present(alert, animated: true, completion: nil)
-                }
+        if currentQuestionIndex < quizQuestions.count {
+            displayQuestion()
+            resetTimer()
+            startTimer()
+        } else {
+            endQuiz()
+        }
+        updateScoreLabel()
+    }
+    
+    func updateScoreLabel() {
+        scoreLabel.text = "Score: \(score)"
+    }
+    
+    func startTimer() {
+        timeRemaining = 10 // Reset time for each question
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        timeRemaining -= 1
+        if timeRemaining == 0 {
+            timer?.invalidate()
+            checkAnswer()
+        }
+    }
+    
+    func resetTimer() {
+        timer?.invalidate()
+    }
+    
+    func endQuiz() {
+        let alert = UIAlertController(title: "Congratulations!", message: "You have completed the quiz with a score of \(score)!", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+        score = 0 // Reset score for next time
+        currentQuestionIndex = 0 // Reset question index for next time
+    }
+    
+    @IBAction func checkAnswerButtonPressed(_ sender: UIButton) {
+        resetTimer()
+        checkAnswer()
+    }
+}
 
     /*
     // MARK: - Navigation
